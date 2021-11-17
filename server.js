ae00a6d9c67f4fc96ed85836b97ef27087ae53eb
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-11-16 20:58:11 -0500

    added inquirer

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const sequelize = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,18 +9,18 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    // MySQL username,
-    user: process.env.DB_USER,
-    // MySQL password
-    password: process.env.DB_PASSWORD,
-    database: "books_db",
-  },
-  console.log(`Connected to the books_db database.`)
-);
+// const db = mysql.createConnection(
+//   {
+//     host: "localhost",
+//     // MySQL username,
+//     user: process.env.DB_USER,
+//     // MySQL password
+//     password: process.env.DB_PASSWORD,
+//     database: "company_db",
+//   },
+//   console.log(`Connected to the company_db database.`)
+// );
 
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
 });
