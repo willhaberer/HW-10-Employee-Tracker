a41a76a5ee53126ba940835093c6c2df8556ba6b
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-11-16 20:45:08 -0500

    created .env file

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,18 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: process.env.DB_USER,
+    // MySQL password
+    password: process.env.DB_PASSWORD,
+    database: "books_db",
+  },
+  console.log(`Connected to the books_db database.`)
+);
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
