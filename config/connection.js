cc802991242704b132f3ac81e4f0090087e4198b
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-11-16 21:59:02 -0500

    added README

--- config/connection.js
+++ config/connection.js
@@ -8,8 +8,7 @@ const sequelize = new Sequelize(
     host: "localhost",
     dialect: "mysql",
     port: 3306,
-  },
-  console.log(`Connected to the company_db database.`)
+  }
 );
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -10,16 +10,43 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-index().then(function (userTask) {
+// const db = mysql.createConnection(
+//   {
+//     host: "localhost",
+//     MySQL username,
+//     user: "root",
+//     MySQL password
+//     password: "trilogy!",
+//     database: "courses_db",
+//   },
+//   console.log(`Connected to the courses_db database.`)
+// );
+
+function userInput() {
+  var userTask = index();
+  console.log(userTask);
   switch (userTask) {
     case "View All Departments":
+      console.log("about to run view department");
       viewAllDepartments();
       break;
     default:
       console.log("Unexpected Error Please Try Again");
   }
-});
+}
+
+function viewAllDepartments() {
+  sequelize.query(
+    "SELECT department.id, department.department_name FROM department;",
+    function (err, res) {
+      if (err) throw err;
+      console.log(res);
+    }
+  );
+}
 
 sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
+
+userInput();
